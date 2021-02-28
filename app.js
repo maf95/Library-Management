dd512018a48957648ff5e6c84f71f3a1826dd7aa
maf95 <catalinmaf@yahoo.com>
2021-02-28 21:19:19 +0430

    finished index page

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const admin = require("./routes/admin");
+const path = require("path");
+const { static } = require("express");
+
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "images")));
+
+app.use(admin);
+
+app.listen(3000, console.log("Server started on port 3000"));

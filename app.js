34711fe4ece7fd9a66e4eddcac60d948a24666a3
maf95 <catalinmaf@yahoo.com>
2021-03-27 22:26:39 +0430

    added csrf protection

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 const nocache = require("nocache");
 const flash = require("connect-flash");
+const csrf = require("csurf");
 const password = process.env.PASSWORD_MONGODB;
 const dbURI =
     "mongodb+srv://catalin:" +
@@ -19,6 +20,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", "views");
+const csrfProtection = csrf();
 
 app.use(nocache());
 app.use(express.static(path.join(__dirname, "public")));
@@ -37,19 +39,26 @@ app.use(
         store: store,
     })
 );
+app.use(csrfProtection);
 app.use(flash());
+app.use((req, res, next) => {
+    res.locals.csrfToken = req.csrfToken();
+    next();
+});
 
 app.use(admin);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-    res
-        .status(500)
-        .render("error/500", {
-            pageTitle: "Error!",
-            error: error,
-            role: req.session.user.role,
-        });
+    let role = "";
+    if (req.session.user) {
+        role = req.session.user.role;
+    }
+    res.status(500).render("error/500", {
+        pageTitle: "Error!",
+        error: error,
+        role: role,
+    });
 });
 
 app.listen(3000, console.log("Server started on port 3000"));
